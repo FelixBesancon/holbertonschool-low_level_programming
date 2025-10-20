8fcbb5b2d8481924ccf419d74d1d1eead83ff8dc
FelixBesancon <f.besancon@hotmail.fr>
2025-10-20 17:17:24 +0000

    Add 6-size file

---
+++ hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * printf - Prints formatted output to stdout
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d bytes,\n");
+	return (0);
+}
