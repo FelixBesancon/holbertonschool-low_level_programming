95938495979ae39e9b58b84d7512b79acf33cbc6
FelixBesancon <f.besancon@hotmail.fr>
2025-10-20 13:52:48 +0000

    Bug fix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * puts - Prints a static phrase on the
+ * monitor, preceded by a quotation mark,
+ * and followed by a new line, using puts
+ * command
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
