233a2438c0925b030bd1a9d463ef4508db3f2dd3
FelixBesancon <f.besancon@hotmail.fr>
2025-10-21 14:37:00 +0000

    Bug fix

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * putchar - Writes a character to stdout
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i=0;
+	for (i=97; i<123; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
