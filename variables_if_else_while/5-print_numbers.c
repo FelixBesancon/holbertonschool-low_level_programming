c6e1e4925cd1cebe6c5b22424121b40568f12e86
FelixBesancon <f.besancon@hotmail.fr>
2025-10-21 17:11:15 +0000

    Add 5-print_numbers program

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * printf - Prints formatted output to stdout
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
