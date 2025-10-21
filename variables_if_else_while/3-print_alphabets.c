0321c89991fbfc2509fa78ec6de68e6aa3614bd2
FelixBesancon <f.besancon@hotmail.fr>
2025-10-21 14:44:47 +0000

    Add 3-print_alphabets program

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * putchar - Writes a character to stdout
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 97; i < 123; i++)
+		putchar(i);
+	for (i = 65; i < 91; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
+
