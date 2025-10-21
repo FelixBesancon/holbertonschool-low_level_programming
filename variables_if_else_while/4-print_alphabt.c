21b31d3d5b5ba91f7076dd0c037460b88838691e
FelixBesancon <f.besancon@hotmail.fr>
2025-10-21 15:10:37 +0000

    Bug fix

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,12 @@ int main(void)
 {
 	int i = 97;
 
-	do {
-		if (i == 101 || i == 113)
-			i++;
-		else
-			putchar(i);
-			i++;
-	} while (i < 123);
+	while (i < 123)
+	{
+	if (i != 101 && i != 113)
+		putchar(i);
+	i++;
+	}
 	putchar(10);
 	return (0);
 }
