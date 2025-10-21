cc51b49e7e4305b1364f8168122e009906cd9b87
FelixBesancon <f.besancon@hotmail.fr>
2025-10-21 08:29:58 +0000

    Bug fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,11 +2,16 @@
 /**
  * main - Entry point of the program
  * printf - Prints formatted output to stdout
+ * sizeof - Returns the size in bytes of a type or variable
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("Size of a char: %d bytes,\n");
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
