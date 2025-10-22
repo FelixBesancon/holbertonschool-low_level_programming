aa88791f18774fe851c51d1c418c731299a4677c
FelixBesancon <f.besancon@hotmail.fr>
2025-10-22 13:15:19 +0000

    Add 6-abs.c function

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - Return the absolute value of an integer
+ * @r: The integer to check
+ *
+ * Return: -r if r is negative, r otherwise
+ */
+int _abs(int r)
+{
+	if ( r<0 )
+		return (-r);
+	else
+		return (r);
+}
