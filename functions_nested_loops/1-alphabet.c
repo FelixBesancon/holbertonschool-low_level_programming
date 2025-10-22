837d440972e7b858b0d655f6033a123bbdbabc4c
FelixBesancon <f.besancon@hotmail.fr>
2025-10-22 09:40:56 +0000

    Add 1-alphabet.c program

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point of the program
+ * _putchar - Writes a character to stdout
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 97; i < 123; i++)
+		_putchar(i);
+	_putchar(10);
+	return (0);
+}
