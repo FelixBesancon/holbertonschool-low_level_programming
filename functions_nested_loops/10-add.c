f4cedb96f7d0af40fa7eaf0276512374b93bca8e
FelixBesancon <f.besancon@hotmail.fr>
2025-10-23 10:20:41 +0000

    Add 10-add.c function

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @l: First integer
+ * @m: Second integer
+ *
+ * Return: n = l + m
+ */
+int add(int l, int m)
+{
+	int n = (l + m);
+
+	return (n);
+}
