80bbd4795a2f93970050b2749516d6bef810114b
FelixBesancon <f.besancon@hotmail.fr>
2025-10-24 12:36:02 +0000

    Bug fix

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -13,7 +13,7 @@ int main(void)
 	long fibonacci_1 = 1;
 	int n = 0;
 
-	for (n = 1; n != 50; n++)
+	for (n = 1; n <= 50; n++)
 	{
 		fibonacci_result = (fibonacci_0 + fibonacci_1);
 		fibonacci_0 = fibonacci_1;
