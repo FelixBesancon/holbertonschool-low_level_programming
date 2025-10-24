8c9f2443672712cfd7378113c8635a67a08c548a
FelixBesancon <f.besancon@hotmail.fr>
2025-10-24 12:31:06 +0000

    Add 102-fibonacci.c program

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -43,7 +43,7 @@ void print_times_table(int n)
 					_putchar(z % 10 + '0');
 				}
 			}
-			_putchar(10);
+			_putchar('\n');
 			y++;
 		}
 	}
---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * printf - Prints formated output to stdout
+ *
+ * Return: Always o (Success)
+ */
+int main(void)
+{
+	long fibonacci_result = 0;
+	long fibonacci_0 = 0;
+	long fibonacci_1 = 1;
+	int n = 0;
+
+	for (n = 1; n != 50; n++)
+	{
+		fibonacci_result = (fibonacci_0 + fibonacci_1);
+		fibonacci_0 = fibonacci_1;
+		fibonacci_1 = fibonacci_result;
+		printf("%ld\n", fibonacci_result);
+	}
+	return (0);
+}
