0c06c4615e8dc2e339de6ab10b9f00f6b378eb11
FelixBesancon <f.besancon@hotmail.fr>
2025-10-24 11:25:25 +0000

    Bug fix

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table, starting with 0
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int x = 0;
+	int y = 0;
+	int z = 0;
+
+	if (n >= 0 && n <= 15)
+	{
+		while (y <= n)
+		{
+			x = 0;
+			z = x * y;
+			_putchar(z + '0');
+			for (x = 1; x <= n; x++)
+			{
+				z = x * y;
+				if (z <= 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(z + '0');
+				}
+				else if (z <= 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(z / 10  + '0');
+					_putchar(z % 10 + '0');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(z / 100 + '0');
+					_putchar((z % 100) / 10 + '0');
+					_putchar(z % 10 + '0');
+				}
+			}
+			_putchar(10);
+			y++;
+		}
+	}
+}
