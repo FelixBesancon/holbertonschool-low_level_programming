4c1c2e357dd17d8d4d1d7c1b326833bcae0e372a
FelixBesancon <f.besancon@hotmail.fr>
2025-10-27 09:35:42 +0000

    Add 2-mul function

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+ * mul - multiplies two integers
+ * @a: the first integer to multiply
+ * @b: the second integer to multiply
+ *
+ * Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
