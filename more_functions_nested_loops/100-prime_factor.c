ef58b271cb0fefb8211920455910db16d8d3cbe9
FelixBesancon <f.besancon@hotmail.fr>
2025-10-27 16:24:12 +0000

    Add 100-prime_factor program

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - find and prints the largest prime factore
+ * of the number 612852475143 followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long n = 612852475143;
+	unsigned long divisor = 2;
+
+	while (n > 1)
+	{
+		while (n % divisor != 0)
+			
+			divisor++;
+		n = n / divisor;
+			
+	}
+	printf("%lu\n", divisor);
+	return (0);
+}
