12939c122419f5cf6a9bc50efcb8c34cdf82280b
FelixBesancon <f.besancon@hotmail.fr>
2025-10-27 09:27:37 +0000

    Bug fix

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -5,9 +5,9 @@
  * Return: On success 1
  * On else, 0 is returned
  */
-int _isdigit(int c);
+int _isdigit(int c)
 {
-	if (c >=48 && c <= 57)
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
