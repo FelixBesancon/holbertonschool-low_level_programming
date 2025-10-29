1bd26d3f1df62dbe11545f243f2878982887b540
FelixBesancon <f.besancon@hotmail.fr>
2025-10-29 14:44:31 +0000

    Add 6-puts function

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - Prints every other character of a string.
+ * @str: The string to check.
+ *
+ * Return: Void
+ */
+void puts2(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (str[length] != '\0')
+	length++;
+	for (i = 0; i < length; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
