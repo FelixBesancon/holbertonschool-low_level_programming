46d06c962ff3130dbcfde0098954ffb12f404b75
FelixBesancon <f.besancon@hotmail.fr>
2025-10-29 13:06:08 +0000

    Bug fix

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,8 +9,9 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
-	for (i = i; i > 0; i--)
+	while (s[i] != '\0')
+		i++;
+	for (; i > 0; i--)
 		_putchar(s[i]);
 	_putchar('\n');
 }
