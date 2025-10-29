b22d21ff568c12df3bfb91cbdf718d0d8cb79db1
FelixBesancon <f.besancon@hotmail.fr>
2025-10-29 17:48:11 +0000

    Add 100-atoi function

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,36 @@
+/**
+ * _atoi - Convert a string to an integer.
+ * @s: The string to convert.
+ *
+ * Return: The converted integer
+ */
+int _atoi(char *s)
+{
+	int converted = 0;
+	int minus = 0;
+	int i = 0;
+	int number = 0;
+
+	while (s[i] != '\0' && number == 0)
+	{
+		if (s[i] == '-')
+		{
+			minus++;
+			i++;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			number = 1;
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				converted = (converted * 10) + (s[i] - '0');
+				i++;
+			}
+			if (minus % 2 != 0)
+				converted = -converted;
+		}
+		else
+			i++;
+	}
+	return (converted);
+}
