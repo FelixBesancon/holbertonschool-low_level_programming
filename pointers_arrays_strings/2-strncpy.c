398982c7b98722e9b2994bdf1c09b90cf4388e60
FelixBesancon <f.besancon@hotmail.fr>
2025-10-30 15:34:46 +0000

    Add 2-strncpy.c function

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -7,25 +7,18 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0, j = 0;
+	int i = 0;
 
-	while (src[i] != '\0')
-		i++;
-	if (i >= n)
-	{
-		i = n;
-		while (i > 0)
+	while (src[i] != '\0' && i < n)
 		{
-			dest[j] = src[j];
-			i--;
-			j++;
+			dest[i] = src[i];
+			i++;
 		}
-		return (dest);
-	}
-	else
-	{
-		
-	}
-
-	}
+	while (i < n)
+		{
+			dest[i] = '\0';
+			i++;
+		}
+	return (dest);
+	
 }
