ff0c20f2d772e8e46a902f1e9b132f3950ca5fba
FelixBesancon <f.besancon@hotmail.fr>
2025-10-31 08:32:51 +0000

    Add 3-strcmp.c function

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+/**
+ * _strcmp - compares two strings.
+ * @s1: the first string to compare.
+ * @s2: the second string to compare.
+ *
+ * Return: The byte size comparison between the last character compared
+ * of the two strings
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+		i++;
+	return ((s1[i] + '0') - (s2[i] + '0'));
+}
