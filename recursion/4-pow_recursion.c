c992869b887ecfb6a87fd9cc698e25e4e624e33d
FelixBesancon <f.besancon@hotmail.fr>
2025-11-04 13:21:26 +0000

    Add 4-pow_recursion.c function

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+/** _pow_recursion - Returns the value of x raised to the power of y.
+ * @x: The integer to raise to the power of y.
+ * @y: The power to raise the value of x.
+ *
+ * Return: The result of x power y.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	return (x * _pow_recursion(x, y - 1));
+}
+
