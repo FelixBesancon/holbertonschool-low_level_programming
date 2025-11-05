e8d6180b8d979b79c4cadb1f641f0ddfeba465dd
FelixBesancon <f.besancon@hotmail.fr>
2025-11-05 13:23:00 +0000

    Add 10-set_string.c function

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+/**
+ * set_string - sets the value of a pointer to a char.
+ * @s: The pointer to replace with to.
+ * @to : The pointer that replace s.
+ *
+ * Return: Void.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
