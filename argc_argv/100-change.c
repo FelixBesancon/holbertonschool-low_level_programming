5f0f6f7e4903e7eef8738897bbbc91a2fdab8570
FelixBesancon <f.besancon@hotmail.fr>
2025-11-06 13:42:24 +0000

    Add 100-change.c program

---
+++ argc_argv/100-change.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the minimum number of coins to make change
+ * for an amount of money passed in argument.
+ * @argc: The arguments count.
+ * @argv: The argument vector.
+ *
+ * Return: Always 0 (success).
+ */
+int main(int argc, char **argv)
+{
+	int change, coins;
+
+	if (argc == 2)
+	{
+		change = atoi(argv[1]);
+		coins = change / 25;
+		change %= 25;
+		coins += change / 10;
+		change %= 10;
+		coins += change / 5;
+		change %= 5;
+		coins += change / 2;
+		change %= 2;
+		coins += change / 1;
+		printf("%d\n", coins);
+	}
+	return 0;
+}
