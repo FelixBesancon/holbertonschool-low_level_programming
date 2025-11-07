b596b757152a64a34254b3ca798ae56503306664
FelixBesancon <f.besancon@hotmail.fr>
2025-11-07 08:40:58 +0000

    Add 102/magic.c

--- pointers_arrays_strings/102-magic.c
+++ pointers_arrays_strings/102-magic.c
@@ -16,8 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-   printf("adresses = %p %p %p\n", &a[2], &n, (void*)p + 20);
-  *(p + 20) = 98;
+  *(p + 5) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
