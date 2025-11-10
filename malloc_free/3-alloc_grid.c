11a0e681b4f5813252541a4dff22d0b9506c2bdb
FelixBesancon <f.besancon@hotmail.fr>
2025-11-10 15:07:35 +0000

    Add 3-alloc_grid.c function

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,6 +11,8 @@ int **alloc_grid(int width, int height)
 	int **grid = NULL;
 	int i, j;
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
 	grid = malloc(height * sizeof(int *));
 	if (grid == NULL)
 		return (NULL);
