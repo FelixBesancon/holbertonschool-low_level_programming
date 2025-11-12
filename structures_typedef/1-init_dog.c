de4270b0ab22e0e3d3d96579a5b74a27da679f46
FelixBesancon <f.besancon@hotmail.fr>
2025-11-12 21:08:45 +0000

    Bug fix

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 /**
  * init_dog - Initializes a variable of type struct dog.
  * @d: A pointer to the dog to initalize.
@@ -10,7 +11,10 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
