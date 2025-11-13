4f16d9e07e1cf8f503c88bfd2c7ab9e475aa872c
FelixBesancon <f.besancon@hotmail.fr>
2025-11-13 12:46:40 +0000

    Add 4-new_dog.c function

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,30 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+ * _strdup - Returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as parameter.
+ * @str: The string to duplicate to a new string.
+ *
+ * Return: A pointer to a new string, return NULL if str = NULL.
+ */
+char *_strdup(char *str)
+{
+	char *duplicate = NULL;
+	unsigned int i = 0, j = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	duplicate = malloc(sizeof(char) * i + 1);
+	if (duplicate == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+		duplicate[j] = str[j];
+	duplicate[j] = '\0';
+	return (duplicate);
+}
+/**
  * new_dog - Creates a new dog using dog_t type and dog structure.
  * @name: The name of the dog.
  * @age: The age of the dog.
@@ -10,36 +34,23 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	unsigned int i, j;
 	dog_t *dog = malloc(sizeof(dog_t));
 
 	if (dog == NULL)
 		return (NULL);
-	i = 0;
-	while (name[i] != '\0')
-		i++;
-	dog->name = malloc(sizeof(char) * i + 1);
+	dog->name = _strdup(name);
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	for (j = 0; j < i; j++)
-		dog->name[j] = name[j];
-	dog->name[j] = '\0';
 	dog->age = age;
-	i = 0;
-	while (owner[i] != '\0')
-		i++;
-	dog->owner = malloc(sizeof(char) * i + 1);
+	dog->owner = _strdup(owner);
 	if (dog->owner == NULL)
 	{
 		free(dog->name);
 		free(dog);
 		return (NULL);
 	}
-	for (j = 0; j < i; j++)
-		dog->owner[j] = owner[j];
-	dog->owner[j] = '\0';
 	return (dog);
 }
