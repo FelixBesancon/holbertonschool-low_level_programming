e973f2ee21656d7dcb339f7cb2cf72d5e3cd01c4
FelixBesancon <f.besancon@hotmail.fr>
2025-11-17 15:17:45 +0000

    Add 3-get_op_functions.c 3-main.c 3-op_functions.c and calc.h functions, program and header

---
+++ function_pointers/3-main.c
@@ -0,0 +1,22 @@
+#include "calc.h"
+/**
+ * main - Perform simple operations,
+ * operator and numbers given in arguments.
+ * @argc: The arguments count.
+ * @argv: The argument vector.
+ *
+ * Return: Always 0 (success).
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 4)
+	{
+		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+		return (0);
+	}
+	else
+	{
+		printf("Erreur\n");
+		exit(98);
+	}
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "calc.h"
+/**
+ * op_add - Adds two integers
+ * @a: The first integer to add.
+ * @b: The second integer to add.
+ *
+ * Return: The result of the addition.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Substracts two integers
+ * @a: The first integer to substract from.
+ * @b: The second integer to substract to.
+ *
+ * Return: The result of the substraction.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Multiplies two integers
+ * @a: The first integer to multiply.
+ * @b: The second integer to multiply.
+ *
+ * Return: The result of the multiplication.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Divides two integers
+ * @a: The first integer to divide from.
+ * @b: The second integer to divide to.
+ *
+ * Return: The result of the division, exit 100 if b is null.
+ */
+int op_div(int a, int b)
+{
+	if (b != 0)
+		return (a / b);
+	printf("Erreur\n");
+	exit(100);
+}
+/**
+ * op_mod - Modulates two integers
+ * @a: The first integer to modulate from.
+ * @b: The second integer to modulate to.
+ *
+ * Return: The result of the modulo, exit 100 if b is null.
+ */
+int op_mod(int a, int b)
+{
+	if (b != 0)
+		return (a % b);
+	printf("Erreur\n");
+	exit(100);
+}
