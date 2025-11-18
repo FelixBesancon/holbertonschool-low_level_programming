39e9d64edf469bbc132f3d9398c47984615a732f
FelixBesancon <f.besancon@hotmail.fr>
2025-11-18 10:58:31 +0000

    Add 0-sum_them_all.c function

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list count;
+
+	if (n == 0)
+		return (0);
+	va_start(count, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(count, int);
+	}
+	va_end(count);
+	return (sum);
+}
