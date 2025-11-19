2ec5607716142faa9f8b68eb063f2f40c24c4a97
FelixBesancon <f.besancon@hotmail.fr>
2025-11-19 08:18:42 +0000

    Use printf in 1-print_numbers.c function

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -10,39 +10,20 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int count = n;
-	int number, i, j;
+	int number;
 	va_list numbers;
 
-	if (count == 0 || separator == NULL)
+	if (count == 0)
 		return;
 	va_start(numbers, n);
 	while (count > 0)
 	{
 		number = va_arg(numbers, int);
-		i = 1;
-		j = (number / 10);
-		while (j != 0)
-		{
-			j = j / 10;
-			i = i * 10;
-		}
-		if (number < 0)
-			putchar('-');
-		for (j = 0; i > 0; i = (i / 10))
-		{
-			if (number >= 0)
-			{
-				j = number / i;
-				number = number - (j * i);
-			}
-			else
-			{
-				j = -(n / i);
-				number = number + (j * i);
-			}
-			putchar(j + '0');
-			count--;
-		}
-		va_end(numbers);
+		printf("%d", number),
+		count--;
+		if (count > 0 && separator != NULL)
+			printf("%s", separator);
 	}
+	printf("\n");
+	va_end(numbers);
 }
