b87fbe57c8ce3752152b6f3ae9b8d91b4661c3b9
FelixBesancon <f.besancon@hotmail.fr>
2025-12-01 15:05:44 +0000

    cade rvision for tasks 0, 1, 2

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -7,28 +7,17 @@
  *
  * Return: The number of nodes.
  */
-
 size_t print_list(const list_t *h)
 {
 	size_t nodes;
-	char *current_str = h->str;
-	unsigned int current_len = h->len;
-	const list_t *current_list = h;
 
-	for (nodes = 0; current_list != NULL; nodes++)
+	for (nodes = 0; h != NULL; nodes++)
 	{
-		if (current_list->str == NULL)
-		{
-			current_str = strdup("(nil)");
-			current_len = 0;
-		}
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 		else
-		{
-			current_str = current_list->str;
-			current_len = current_list->len;
-		}
-		printf("[%u] %s\n", current_len, current_str);
-		current_list = current_list->next;
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
 	}
 	return (nodes);
 }
--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -7,9 +7,8 @@
 size_t list_len(const list_t *h)
 {
 	size_t nodes;
-	const list_t *current_list = h;
 
-	for (nodes = 0; current_list != NULL; nodes++)
-		current_list = current_list->next;
+	for (nodes = 0; h != NULL; nodes++)
+		h = h->next;
 	return (nodes);
 }
